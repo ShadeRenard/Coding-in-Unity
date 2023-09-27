44a2710674f7b3b71299cf29413ccc7ae7c2d3d7
Sadie <erichsensadie@gmail.com>
2023-09-27 15:37:01 -0600

    inspector

---
+++ file/Assets/Inspector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inspector : MonoBehaviour
+{
+    public string myName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive and my name is " + myName)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
