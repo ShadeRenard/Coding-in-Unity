141815760b36317158465af81e7d6076e6d10a6c
Sadie <erichsensadie@gmail.com>
2023-09-27 19:43:10 -0600

    MainPlayerScript

--- file/Assets/MainPlayerScript.cs
+++ file/Assets/MainPlayerScript.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainPlayer : MonoBehaviour
+public class MainPlayerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("I am alive!");
+        
     }
 
     // Update is called once per frame
