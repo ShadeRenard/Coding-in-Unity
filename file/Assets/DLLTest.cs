e23ebc2f58034c085a019a07f7ff5d578f8bc9f6
Sadie <erichsensadie@gmail.com>
2023-09-27 18:44:20 -0600

    DLLTest

---
+++ file/Assets/DLLTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLLTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    public int c;
+    public void AddValues(int a, int b)
+    c = a + b;
+
+}
+
+public static int GenerateRandom(int min, int max)
+{
+    System.Random rand = new System.Random();
+    return rand.Next(min, max);
+}
+
+// Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
