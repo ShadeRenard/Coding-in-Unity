8627e5cbea79ea550155be9562d7e8fe2d7aec44
Sadie <erichsensadie@gmail.com>
2023-09-28 23:32:37 -0600

    New script

--- Player moving/Assets/Scripts/Test.cs
+++ Player moving/Assets/Scripts/Test.cs
@@ -2,28 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class PlayerMovement; MonoBehavior
 {
-    Rigidbody2D _rb;
-    
-    float _walkSpeed;
-    float _inputHorizontal;
-    
+public float speed;
+public float rotationSpeed;
+public float jumpSpeed;
+
+private CharacterController characterController;
+private float ySpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        _rb - gameObject.GetComponent<Rigibody2D>();
-
-        _walkSpeed = 5.5f;
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _inputHorizontal = _inputHorizontal.GetAxisRaw("Horizontal");
-        if (_inputHorizontal != 0)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
         {
-            _rb.AddForce(new Vector2(_inputHorizontal * _walkSpeed, 0f));
+            ySpeed = jumpSpeed;
+        }
+
+        characterController.SimpleMove(movementDirection * magnitude)
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.wp);
+
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
+// /
+// / Rigidbody2D _rb;
+    
+    // float _walkSpeed;
+    // float _inputHorizontal;
+    
+    // ACTUAL COMMENT Start is called before the first frame update
+    // void Start()
+    // {
+        // _rb - gameObject.GetComponent<Rigibody2D>();
+
+        // _walkSpeed = 5.5f;
+    // }
+
+    // ACTUAL COMMENT Update is called once per frame
+    // void Update()
+    // {
+        // _inputHorizontal = _inputHorizontal.GetAxisRaw("Horizontal");
+        // if (_inputHorizontal != 0)
+        // {
+            // _rb.AddForce(new Vector2(_inputHorizontal * _walkSpeed, 0f));
+        // }
+    // }
+// }
