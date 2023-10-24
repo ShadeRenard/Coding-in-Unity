f1924741a3105681f81b255a72f5e8dc340c644c
ShadeRenard <erichsensadie@gmail.com>
2023-10-24 14:34:52 -0600

    Scripts Lab

---
+++ Scripts Lab/Assets/FloatData/FloatData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+  public float value; 
+
+  public void UpdateValue(float num)
+  {
+    value += num;
+  }
+}
+
---
+++ Scripts Lab/Assets/FloatData/TextLabelBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextLabelBehaviour : MonoBehaviour
+{
+  public Text label;
+  public FloatData dataObj;
+    private void Start()
+    {
+        label = GetComponent<Text>();
+        UpdateLabel();
+    }
+
+    public void UpdateLabel () 
+    {
+        label.text = dataObj.value.ToString();
+    }
+}
