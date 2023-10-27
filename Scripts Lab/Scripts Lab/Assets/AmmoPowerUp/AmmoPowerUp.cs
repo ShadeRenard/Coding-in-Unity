daf366cfd73746e93b7bbae797dbde4bd7a40abf
ShadeRenard <erichsensadie@gmail.com>
2023-10-26 18:49:41 -0600

    Ammo added

---
+++ Scripts Lab/Scripts Lab/Assets/AmmoPowerUp/AmmoPowerUp.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+
+public class AmmoPowerUp : MonoBehaviour
+  {
+       [SerializeField] private IntData ammoValue;
+   
+       private void OnTriggerEnter(Collider other)
+       {
+           if (other.CompareTag("Player"))
+           {
+               // Add ammo to the player's weapon
+               AddAmmo(other.gameObject);
+   
+               // Destroy the power-up game object
+               Destroy(gameObject);
+           }
+       }
+   
+       private void AddAmmo(GameObject player)
+       {
+           // Retrieve the player's weapon component (e.g., WeaponController script)
+           WeaponController weaponController = player.GetComponent<WeaponController>();
+   
+           // Check if the player has a weapon component
+           if (weaponController != null)
+           {
+               // Add the ammo value to the player's current ammo count
+               weaponController.AddAmmo(ammoValue.Value);
+           }
+       }
+   }
---
+++ Scripts Lab/Scripts Lab/Assets/AmmoPowerUp/WeaponControler.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponControler : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 25;
+
+    public float xRange = 30;
+
+    public Transform blaster;
+    public GameObject lazer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set HorizontalInput to receive values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moves Player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // Keeps player within bounds
+        // Left Side Wall
+        if(transform.position.x < -xRange )
+        {
+            transform.position = new Vector3(-xRange,transform.position.y, transform.position.z);
+        }
+        // Right Side Wall
+        if(transform.position.x > xRange )
+        {
+            transform.position = new Vector3(xRange,transform.position.y, transform.position.z);
+        }
+        // if space bar is pressed fire
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Creates laser at the blaster transform position maintaining the objects rotation
+            Instantiate(lazer, blaster.transform.position, lazer.transform.rotation);
+        }
+
+    }
+
+    // Delete any object with a tigger that hits the player
+    private void OnTriggerEnter(Collider other)
+    {
+            Destroy(other.gameObject);
+    }
+
+
+}
+
